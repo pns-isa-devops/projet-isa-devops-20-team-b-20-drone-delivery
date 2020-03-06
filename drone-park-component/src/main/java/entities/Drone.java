c386b5e63bee656937746164cadb6c3e9d2d495b
Betsara Marcellin <betsara.marcellin@etu.unice.fr>
2020-03-06 11:26:26 +0100

    pns-isa-devops/projet-isa-devops-20-team-b-20#9 serialize Drone

--- drone-park-component/src/main/java/entities/Drone.java
+++ drone-park-component/src/main/java/entities/Drone.java
@@ -4,8 +4,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Objects;
 
-public class Drone{
+
+public class Drone implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -34,6 +37,21 @@ public class Drone{
 
     public void setDroneStatus(DroneStatus droneStatus) {
         this.droneStatus = droneStatus;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this) return true;
+
+        if (obj != null)
+            return (obj.hashCode() == Objects.hash(this.droneId, this.droneStatus));
+
+        return false;
     }
 
 

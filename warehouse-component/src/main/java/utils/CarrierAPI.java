db97aa95f98c9d9455196b7a974f1e6a9129275d
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2020-03-17 20:50:41 +0100

    pns-isa-devops/projet-isa-devops-20-team-b-20-drone-delivery#3 update api to add mocked value

--- warehouse-component/src/main/java/utils/CarrierAPI.java
+++ warehouse-component/src/main/java/utils/CarrierAPI.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import entities.Delivery;
 import entities.DeliveryStatus;
 import entities.Parcel;
@@ -10,6 +13,8 @@ import entities.Parcel;
 public class CarrierAPI {
 
     private String url;
+    private List<Parcel> parcels;
+    private List<Delivery> deliveries;
 
     /**
      * not to be used for now (mocked value)
@@ -19,6 +24,31 @@ public class CarrierAPI {
      */
     public CarrierAPI(String host, String port) {
         this.url = "http://" + host + ":" + port;
+        parcels = new ArrayList<>();
+        Parcel p = new Parcel();
+        p.setAddress("2255 route des Dolines, 06560 Valbonne");
+        p.setCarrier("JPP");
+        p.setCustomerName("Jasmine");
+        p.setParcelNumber("jpp106");
+        parcels.add(p);
+        p = new Parcel();
+        p.setAddress("5522 avenue des Moutons, 06560 Valbonne");
+        p.setCarrier("JPP2");
+        p.setCustomerName("Arnold");
+        p.setParcelNumber("jpp206");
+        parcels.add(p);
+        deliveries = new ArrayList<>();
+        Delivery d = new Delivery();
+        d.setDeliveryNumber("del106");
+        d.setParcel(parcels.get(0));
+        d.setPriceToCharge(15.60);
+        d.setStatus(DeliveryStatus.NOT_DELIVERED);
+        deliveries.add(d);
+        d = new Delivery();
+        d.setDeliveryNumber("del206");
+        d.setPriceToCharge(10.60);
+        d.setStatus(DeliveryStatus.NOT_DELIVERED);
+        deliveries.add(d);
     }
 
     /**
@@ -30,20 +60,21 @@ public class CarrierAPI {
 
     public Parcel getParcelInformation(String parcelNumber) {
         // just to test
-        Parcel p = new Parcel();
-        p.setAddress("address");
-        p.setCarrier("carrier");
-        p.setCustomerName("customerName");
-        p.setParcelNumber("parcelNumber");
-        return p;
+        // @formatter:off
+        return parcels.stream()
+                    .filter(p -> p.getParcelNumber().equals(parcelNumber))
+                    .findAny()
+                    .orElse(null);
+        // @formatter:on
     }
 
     public Delivery getDeliveryInformation(String deliveryNumber) {
         // just to test
-        Delivery d = new Delivery();
-        d.setDeliveryNumber("deliveryNumber");
-        d.setPriceToCharge(10.2);
-        d.setStatus(DeliveryStatus.NOT_DELIVERED);
-        return d;
+        // @formatter:off
+        return deliveries.stream()
+                    .filter(d -> d.getDeliveryNumber().equals(deliveryNumber))
+                    .findAny()
+                    .orElse(null);
+        // @formatter:on
     }
 }

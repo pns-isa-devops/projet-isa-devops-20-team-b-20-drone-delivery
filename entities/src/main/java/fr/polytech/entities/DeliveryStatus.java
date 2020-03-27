fd8f5ce31d9844fca5b9ec77d4a4575d7e6a0c66
Younes Abdennadher <younes.abdennadher@etu.univ-cotedazur.fr>
2020-03-27 12:29:11 +0100

    pns-isa-devops/projet-isa-devops-20-team-b-20#33 Adding git subrepositories for each modules.

--- entities/src/main/java/fr/polytech/entities/DeliveryStatus.java
+++
@@ -1,5 +0,0 @@
-package fr.polytech.entities;
-
-public enum DeliveryStatus {
-    FAILED, NOT_DELIVERED, ONGOING, DELIVERED,;
-}
--- entities/src/main/java/fr/polytech/entities/TimeState.java
+++
@@ -1,5 +0,0 @@
-package fr.polytech.entities;
-
-public enum TimeState {
-    DELIVERY, CHARGING, REVIEW, UNAVAILABLE
-}
